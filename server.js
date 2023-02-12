25f4866b28c0814356ea8c2472f57e5bb2fc0980
Yago Sampaio <yagosampaio@gmail.com>
2023-02-11 18:58:45 -0700

    Week 6

--- server.js
+++ server.js
@@ -8,18 +8,36 @@ const bodyParser = require ("body-parser");
 
 const {v4: uuidv4} = require('uuid'); //universely unique identifier
 
+const Redis = require('redis'); // The library
+
+const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"}); //This points to redis
+
 app.use(bodyParser.json()); // This looks for incoming data
+
+app.use(express.static('public'));
+const cookieParser = require("cookie-parser");
+app.use(cookieParser);
 
 app.get("/",(req, res) => {
     res.send("Hello Yago");
 });
 
-app.post('/login', (req,res) =>{
+app.get("/validate", async (req,res)=>{
+    const loginToken = req.cookies.stedicookie;
+    console.log("loginToken", loginToken);
+    const loginUser = await redisClient.hGet("TokenMap",loginToken);
+    res.send(loginUser);
+});
+
+app.post('/login', async (req,res) =>{
     const loginUser = req.body.userName;
     const loginPassword = req.body.password; //Access the password data in the body
     console.log('Login username: ' +loginUser);
-    if (loginUser=="yago@gmail.com" && loginPassword=="Password123!"){
+    const correctPassword = await redisClient.hGet('UserMap',loginUser); //gets correct password from redis
+    if (correctPassword==loginPassword){
         const loginToken = uuidv4();
+        await redisClient.hSet("TokenMap",loginToken,loginUser);
+        res.cookie("stedicookie",loginToken);
         res.send(loginToken);
     } else {
         res.status(401);//unauthorized
@@ -27,7 +45,7 @@ app.post('/login', (req,res) =>{
     }   
 });
 
-
 app.listen(port, () => {
+    redisClient.connect();
     console.log("listening");
 });
